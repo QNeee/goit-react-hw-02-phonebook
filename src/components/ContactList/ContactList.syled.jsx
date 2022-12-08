d15cfa94f9097ab918b1b9daf33c9f61546590c2
QNeee <vovasagan7@gmail.com>
2022-12-08 09:47:48 +0300

    saddsa

--- src/components/ContactList/ContactList.syled.jsx
+++ src/components/ContactList/ContactList.syled.jsx
@@ -3,4 +3,4 @@ import styled from "@emotion/styled";
 
 export const ContactListUl = styled.ul`list-style:none;width:600px;margin-left:auto;margin-right:auto;`;
 export const ContactListLi = styled.li`color:white;`;
-export const Button = styled.button`background-color:black;color:white;margin-left:20px;`;
+export const Button = styled.button`background-color:black;color:white;margin-left:20px;cursor:pointer;`;
